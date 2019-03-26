b92d4d3ed74a0391ee28e1c812a2902df81fd827
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-03-26 01:23:31 +0100

    Increment version, and add project urls

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
+from collections import OrderedDict
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='spotlight',
-    version='0.1.2',
+    version='0.1.3',
     author='Michiel Doesburg',
     author_email='michiel@moddix.com',
     description='Laravel style input validation for Python.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/mdoesburg/spotlight',
+    project_urls=OrderedDict((
+        ('Documentation', 'https://github.com/mdoesburg/spotlight'),
+        ('Code', 'https://github.com/mdoesburg/spotlight')
+    )),
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=[]
