66f9271644646338f9289207793215b4f81014d3
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-04-08 10:06:08 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='spotlight',
-    version='0.2.0',
+    version='0.2.1',
     author='Michiel Doesburg',
     author_email='michiel@moddix.com',
     description='Laravel style input validation for Python.',
