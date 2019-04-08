7b3830eff9b1b07cae6d181bd05fd1f9a3e6fe40
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-04-08 10:05:50 +0200

    Remove unused errors messages

--- spotlight/errors.py
+++ spotlight/errors.py
@@ -1,16 +1,8 @@
 REQUIRED_ERROR = "The {field} field is required."
-REQUIRED_WITHOUT_ERROR = (
-    "The {field} field is required if the {other} field is absent."
-)
-REQUIRED_WITH_ERROR = (
-    "The {field} field is required if the {other} field is present."
-)
-REQUIRED_IF_ERROR = (
-    "The {field} field is required if the {other} field equals {value}."
-)
-NOT_WITH_ERROR = (
-    "The {field} field can't be present when the {other} field is present."
-)
+REQUIRED_WITHOUT_ERROR = "The {field} field is required if the {other} field is absent."
+REQUIRED_WITH_ERROR = "The {field} field is required if the {other} field is present."
+REQUIRED_IF_ERROR = "The {field} field is required if the {other} field equals {value}."
+NOT_WITH_ERROR = "The {field} field can't be present when the {other} field is present."
 FILLED_ERROR = "The {field} field must not be empty when it is present."
 INVALID_EMAIL_ERROR = "Invalid email address."
 INVALID_URL_ERROR = "Invalid URL."
@@ -36,16 +28,6 @@ JSON_ERROR = "The {field} field must be a valid JSON string."
 LIST_ERROR = "The {field} field must be a list."
 ACCEPTED_ERROR = "The {field} field must be accepted."
 
-UNIQUE_ERROR = "A record with this {field} already exists."
-EXISTS_ERROR = "A record with this {field} does not exists."
-EXISTS_WHERE_ERROR = (
-    "A record with this {field} does not exists for the given {other}."
-)
-
-NO_SESSION_ERROR = (
-    "The rule '{rule}' requires a session, but no session was given to the "
-    "validator."
-)
 RULE_NOT_FOUND = "The '{rule}' rule doesn't exist"
 STARTS_WITH_ERROR = (
     "The {field} field must start with one of the following values: {values}."
