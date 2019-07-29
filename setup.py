c8bc8187753322a78e814d1f6dbe95219c3ee310
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-07-29 18:53:23 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='spotlight',
-    version='0.2.5',
-    author='Michiel Doesburg',
-    author_email='michiel@moddix.com',
-    description='Laravel style input validation for Python.',
+    name="spotlight",
+    version="0.2.6",
+    author="Michiel Doesburg",
+    author_email="michiel@moddix.com",
+    description="Laravel style input validation for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mdoesburg/spotlight',
-    project_urls=OrderedDict((
-        ('Documentation', 'https://github.com/mdoesburg/spotlight'),
-        ('Code', 'https://github.com/mdoesburg/spotlight')
-    )),
-    license='MIT',
+    url="https://github.com/mdoesburg/spotlight",
+    project_urls=OrderedDict(
+        (
+            ("Documentation", "https://github.com/mdoesburg/spotlight"),
+            ("Code", "https://github.com/mdoesburg/spotlight"),
+        )
+    ),
+    license="MIT",
     packages=setuptools.find_packages(),
-    install_requires=[]
+    install_requires=[],
 )
