e5e15972847e9ebc096f103e83aef6813f298273
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-08-05 12:37:58 -0400

    Add and improve exceptions

--- spotlight/exceptions.py
+++ spotlight/exceptions.py
@@ -1,3 +1,17 @@
 class RuleNotFoundError(Exception):
-    def __init__(self, rule):
-        super().__init__(f"The '{rule}' rule does not exist")
+    def __init__(self, rule: str):
+        super().__init__(f"the '{rule}' rule does not exist")
+
+
+class InvalidInputError(Exception):
+    def __init__(self, input_type: type):
+        super().__init__(
+            f"expected 'dict' or an 'object' that can be converted to a 'dict' got '{input_type}'"
+        )
+
+
+class InvalidRulesError(Exception):
+    def __init__(self, rules_type: type):
+        super().__init__(
+            f"expected 'dict' got '{rules_type}'"
+        )
