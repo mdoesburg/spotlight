63826d6fa7fd65462e6ba1cb68c11b278581c08c
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-08-19 13:50:30 -0400

    Add direct validation methods test

--- spotlight/tests/validator_test.py
+++ spotlight/tests/validator_test.py
@@ -16,3 +16,14 @@ class ValidatorPluginTest(unittest.TestCase):
         with mock.patch.object(plugin, "rules", wraps=plugin.rules) as m:
             Validator([plugin])
         m.assert_called()
+
+
+class DirectValidationMethodsTest(ValidatorTest):
+    def test_direct_validation_methods_are_present(self):
+        methods = [attr for attr in dir(Validator) if attr.startswith("valid_")]
+
+        self.assertEqual(len(methods) > 0, True)
+        for name, rule in self.validator._available_rules.items():
+            for attr in dir(rule):
+                if attr.startswith("valid_"):
+                    self.assertEqual(attr in methods, True)
