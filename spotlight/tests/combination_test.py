bc0214fdfdf4bebcd51c34a1413ec28ecaf276d6
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-09-20 18:34:50 +0200

    Add combination test

--- spotlight/tests/combination_test.py
+++ spotlight/tests/combination_test.py
@@ -13,3 +13,15 @@ class CombinationTest(ValidatorTest):
 
         self.assertEqual(errs[0], EMAIL_ERROR.format(field=field))
         self.assertEqual(errs[1], MIN_STRING_ERROR.format(field=field, min=5))
+
+    def test_before_and_after_with_custom_format_expect_no_error(self):
+        rules = {
+            "start_time": "date_time:%H:%M:%S|after:12:00:00|before:end_time",
+            "end_time": "date_time:%H:%M:%S",
+        }
+        data = {"start_time": "12:30:00", "end_time": "13:00:00"}
+        expected = {}
+
+        errors = self.validator.validate(data, rules)
+
+        self.assertEqual(errors, expected)
