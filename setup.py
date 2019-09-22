1ce15391fe3f983d4cb624a3760df62ac9586480
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2019-09-22 09:32:48 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="spotlight",
-    version="1.0.0",
+    version="1.0.1",
     author="Michiel Doesburg",
     author_email="michiel@moddix.com",
     description="Laravel style data validation for Python.",
