6b9615a6433033c8e233e5da4d928d78e7c14950
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2020-10-31 08:57:41 -0400

    Improve setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+import re
+
 from setuptools import setup, find_packages
 from collections import OrderedDict
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("src/spotlight/__init__.py", encoding="utf8") as f:
+    description = f.readline().strip('"\n')
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
 setup(
     name="spotlight",
-    version="2.1.0",
+    version=version,
     author="Michiel Doesburg",
     author_email="michiel@moddix.com",
-    description="Laravel style data validation for Python.",
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="spotlight validation validate",
@@ -25,4 +31,5 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[],
+    python_requires=">=3.6",
 )
