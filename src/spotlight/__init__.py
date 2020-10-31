a7b824b275d4c44c99303e9163bc42dcf8f26971
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2020-10-31 08:41:10 -0400

    Add version and description to init file

---
+++ src/spotlight/__init__.py
@@ -0,0 +1,6 @@
+"""Data validation for Python, inspired by the Laravel framework."""
+
+__version__ = "2.1.0"
+
+from .validator import Validator, Data, Rules, ValidationFunction
+from .rules import Rule
