e5b757c7dc93572999d00e6a6f6400417cf57349
Michiel Doesburg <3763566+mdoesburg@users.noreply.github.com>
2022-11-17 10:16:26 -0500

    Release version 3.0.1

--- src/spotlight/__init__.py
+++ src/spotlight/__init__.py
@@ -1,6 +1,6 @@
 """Data validation for Python, inspired by the Laravel framework."""
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 from .validator import Validator, Data, Rules, ValidationFunction
 from .rules import Rule
